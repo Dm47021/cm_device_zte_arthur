1a3b634d3678e2c8a717e7c1f11b327b86a16f9b
jetx2x <joshua.dixon@live.com>
2013-05-28 10:17:06 -0300

    Update DeviceSettings.java

--- WarpedParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ WarpedParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -8,6 +8,9 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.widget.CheckBox;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.PowerManager;
 
 public class DeviceSettings extends PreferenceActivity {
     
@@ -28,7 +31,7 @@ public class DeviceSettings extends PreferenceActivity {
         switch(view.getId()) {
             case R.id.hwacc:
                 if (checked)
-                    if(android.os.SystemProperties.get(debug.sf.hw) != 1){
+                    if(android.os.SystemProperties.get(debug.sf.hw) == 0){
                         android.os.SystemProperties.set(debug.sf.hw, 1);
                         checkboxChanged();
                     }
@@ -53,6 +56,42 @@ public class DeviceSettings extends PreferenceActivity {
     }
     
     public void checkboxChanged() {
-        //TODO implement later... reboot mechanism of sorts
+        String s = "CAUTION!!!";
+        String t = "Changing this option requires a full reboot. Reboot now?";
+        String y = "Reboot Now";
+        String n = "Not Now";
+        String c = "Cancel";
+        
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+// 
+        builder.setMessage(t)
+               .setTitle(s);
+               
+        builder.setPositiveButton(y, new DialogInterface.OnClickListener() {
+           public void onClick(DialogInterface dialog, int id) {
+               PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+               powerManager.reboot(null);
+           }
+       });
+        builder.setNegativeButton(c, new DialogInterface.OnClickListener() {
+           public void onClick(DialogInterface dialog, int id) {
+               if(android.os.SystemProperties.get(debug.sf.hw) == 0){
+                        android.os.SystemProperties.set(debug.sf.hw, 1);
+                    }
+                else
+                    if(android.os.SystemProperties.get(debug.sf.hw) == 1){
+                        android.os.SystemProperties.set(debug.sf.hw, 0);
+                    }
+           }
+       });
+        builder.setNeutralButton(c, new DialogInterface.OnClickListener() {
+           public void onClick(DialogInterface dialog, int id) {
+               
+           }
+       });
+
+
+        AlertDialog dialog = builder.create();
     }
 }
