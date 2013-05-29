dd32b45902c632081f28e2df9d8c419cb9f7685d
jetx2x <joshua.dixon@live.com>
2013-05-29 09:57:21 -0400

    Update DeviceSettings.java

--- WarpedParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ WarpedParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -12,6 +12,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.PowerManager;
 
+import com.cyanogenmod.settings.device.Utils;
+
 public class DeviceSettings extends PreferenceActivity {
     
     public CheckBox cb = (CheckBox) findViewById(android.R.id.cbacc);
@@ -33,12 +35,12 @@ public class DeviceSettings extends PreferenceActivity {
                 if (checked)
                     if(android.os.SystemProperties.get(debug.sf.hw) == 0){
                         android.os.SystemProperties.set(debug.sf.hw, 1);
-                        checkboxChanged();
+                        Util.checkboxChanged();
                     }
                 else
                     if(android.os.SystemProperties.get(debug.sf.hw) == 1){
                         android.os.SystemProperties.set(debug.sf.hw, 0);
-                        checkboxChanged();
+                        Utils.checkboxChanged();
                     }
                 break;
         }
@@ -53,47 +55,5 @@ public class DeviceSettings extends PreferenceActivity {
             Utils.writeValue("/sys/module/msm_battery/parameters/usb_chg_enable", 1);
         else
             Utils.writeValue("/sys/module/msm_battery/parameters/usb_chg_enable", 0);
-    }
-    
-    public void checkboxChanged() {
-        String s = "CAUTION!!!";
-        String t = "Changing this option requires a full reboot. Reboot now?";
-        String y = "Reboot Now";
-        String n = "Not Now";
-        String c = "Cancel";
-        
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-// 
-        builder.setMessage(t)
-               .setTitle(s);
-               
-        builder.setPositiveButton(y, new DialogInterface.OnClickListener() {
-           public void onClick(DialogInterface dialog, int id) {
-               PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
-               powerManager.reboot(null);
-           }
-       });
-        builder.setNegativeButton(c, new DialogInterface.OnClickListener() {
-           public void onClick(DialogInterface dialog, int id) {
-               if(android.os.SystemProperties.get(debug.sf.hw) == 0){
-                        android.os.SystemProperties.set(debug.sf.hw, 1);
-                        cb.setChecked(true);
-                    }
-                else
-                    if(android.os.SystemProperties.get(debug.sf.hw) == 1){
-                        android.os.SystemProperties.set(debug.sf.hw, 0);
-                        cb.setChecked(false);
-                    }
-           }
-       });
-        builder.setNeutralButton(c, new DialogInterface.OnClickListener() {
-           public void onClick(DialogInterface dialog, int id) {
-               dialog.dismiss();
-           }
-       });
-
-
-        AlertDialog dialog = builder.create();
     }
 }
