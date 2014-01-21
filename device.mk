549761daee9dd6c6cc71812ef3fce03129869ef6
dm47021 <dm47021@hotmail.com>
2014-01-21 05:45:44 -0500

    fix 3g, everything else still working..thank god!! this is because kernel cmdline androidboot.hardware=arthur tells init to initialzie init.arthur, init.qcom is never used in this scenario.

--- device.mk
+++ device.mk
@@ -133,7 +133,6 @@ PRODUCT_COPY_FILES += \
     device/zte/arthur/root1/init.rc:/root/init.rc \
     device/zte/arthur/root1/init.arthur.rc:/root/init.arthur.rc \
     device/zte/arthur/root1/initlogo.rle:/root/initlogo.rle \
-    device/zte/arthur/root1/init.qcom.rc:/root/init.qcom.rc \
     device/zte/arthur/root1/init.qcom.sh:/root/init.qcom.sh \
     device/zte/arthur/root1/logo.bmp:/root/logo.bmp \
     device/zte/arthur/root1/ueventd.rc:/root/ueventd.rc \
