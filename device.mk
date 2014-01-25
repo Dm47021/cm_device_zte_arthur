ad4bb92a7ce7e9885b00fc7895be5cda85f8aded
dm47021 <dm47021@hotmail.com>
2014-01-25 16:30:24 -0500

    updates, fix mic issue

--- device.mk
+++ device.mk
@@ -132,7 +132,6 @@ PRODUCT_COPY_FILES += \
     device/zte/arthur/root1/default.prop:/root/default.prop \
     device/zte/arthur/root1/init.rc:/root/init.rc \
     device/zte/arthur/root1/init.arthur.rc:/root/init.arthur.rc \
-    device/zte/arthur/root1/init.2ndstg.rc:/root/init.2ndstg.rc \
     device/zte/arthur/root1/initlogo.rle:/root/initlogo.rle \
     device/zte/arthur/root1/init.qcom.sh:/root/init.qcom.sh \
     device/zte/arthur/root1/logo.bmp:/root/logo.bmp \
